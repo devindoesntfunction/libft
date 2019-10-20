1e474b0b0b5ad4bde310ca2e841463462795016d
root <root@DESKTOP-N85HELI.localdomain>
2019-10-20 16:24:56 -0700

    removed math because idk how git works and i want it to be a submodule

--- ft_math/floor_log_two.c
+++
@@ -1,4 +0,0 @@
-int	floor_log_two(long double d)
-{
-	return (((*((int*)(((unsigned char*)&d) + 8))) & 0x0000ffff) - 16383);
-}
--- ft_math/ft_ftoa.c
+++
@@ -1,6 +0,0 @@
-#include "../libft.h"
-
-char	*ft_ftoa(double f)
-{
-	return (ft_ldtoa((long double)f));
-}
