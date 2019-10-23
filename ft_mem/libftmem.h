bf6ec0d9fce4ae923da75f6b5211391f27b600fa
root <root@DESKTOP-N85HELI.localdomain>
2019-10-23 01:13:51 -0700

    updated ft_mem/libftmem.h

--- ft_mem/libftmem.h
+++ ft_mem/libftmem.h
@@ -7,5 +7,8 @@ void	ft_memdel(void **ptr);
 void	*ft_bzero(void *ptr, size_t n);
 void	*ft_memnew(size_t n);
 void	*ft_memset(void *dst, int c, size_t n);
+void	*ft_memjoin(void *a, size_t a_len, void *b, size_t b_len);
+void	*ft_memdup(void *ptr, size_t n);
+void	*ft_memmove(void *dst, void *src, size_t n);
 
 #endif
