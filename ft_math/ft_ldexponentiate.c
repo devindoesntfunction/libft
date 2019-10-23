a7cc8236dcf23bb3ee04eaeed98d1780b8813811
root <root@DESKTOP-N85HELI.localdomain>
2019-10-23 01:11:19 -0700

    upgraded ft_ldexponentiate

--- ft_math/ft_ldexponentiate.c
+++ ft_math/ft_ldexponentiate.c
@@ -1,6 +1,18 @@
-long double	ft_ldexponentiate(long double b, unsigned int x)
+typedef long double t_LD;
+
+typedef unsigned int t_UI;
+
+static t_LD	ft_fast_exp(t_LD b, t_UI x, t_LD a)
 {
 	if (x == 0)
-		return (1.0);
-	return (b * ft_ldexponentiate(b, x - 1));
+		return (a);
+	else if (!(x % 2))
+		return (ft_fast_exp(b * b, x / 2, a));
+	else
+		return (ft_fast_exp(b, x - 1, a * b));
+}
+
+long double	ft_ldexponentiate(long double b, unsigned int x)
+{
+	return (ft_fast_exp(b, x, 1.0));
 }
